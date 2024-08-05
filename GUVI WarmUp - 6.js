8cde0e1579301a703d9ec9a2dc92646f00ebc8d9
Yashika-P <yyashika118@gmail.com>
2024-08-05 18:11:45 +0530

    Add files via upload

---
+++ GUVI WarmUp - 6.js
@@ -0,0 +1,18 @@
+/* Write a function that converts hours into seconds. */
+
+function hourToSeconds(hours) {
+	///Your code Starts here 
+	
+	let seconds = (hours * 60) * 60;
+	return (seconds)
+	
+	
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+
+hourToSeconds(24);
+hourToSeconds(10);
+hourToSeconds(2);
+*/
